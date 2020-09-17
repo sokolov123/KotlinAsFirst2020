ffd818563a67fd297fdbac060745c62e76d8ce4a
sokolov123 <anton2003zxcegth@gmail.com>
2020-09-17 22:23:26 +0300

    // надеюсь все будет понятно

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,9 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -18,7 +21,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val last = ((number % 100) / 10) + (number % 10)
+    val first = (number / 1000) + ((number / 100) % 10)
+    return last == first
+}
 
 /**
  * Простая (2 балла)
@@ -27,7 +34,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2))
+}
 
 
 /**
@@ -36,7 +45,15 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    var days = 0
+    when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> days = 31
+        4, 6, 9, 11 -> days = 30
+        2 -> if (year % 100 == 0 && (year / 100) % 4 != 0) days = 28 else if (year % 4 == 0) days = 29 else days = 28
+    }
+    return days
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +65,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val a = sqrt((abs(x1 - x2)).pow(2) + (abs(y1 - y2)).pow(2))
+    return (a + r1 <= r2)
+}
 
 /**
  * Средняя (3 балла)
@@ -59,4 +79,9 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return ((a * b <= r * s) && ((a <= s && b <= r) || (a <= r && b <= s)) ||
+            (a * c <= r * s) && ((a <= s && c <= r) || (a <= r && c <= s)) ||
+            (c * b <= r * s) && ((c <= s && b <= r) || (c <= r && b <= s)))
+    // ух я надеюсь вы поймете где для чего скобки, просто они как раз для понятности происходящего (разграничения условий)
+}
